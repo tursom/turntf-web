3fae62f229967c2f492832a1a836ebbd18c84587
tursom <tursom@foxmail.com>
2026-05-06 17:09:18 +0800

    fix: Express 5 path-to-regexp v8 兼容，catch-all 路由改用 {*splat} 语法

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ const proxy = setupProxy(app, config.backendUrl);
 app.use(express.static(distPath));
 
 // SPA fallback
-app.get("*", (_req, res) => {
+app.get("/{*splat}", (_req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
